e4a509dcbef819935e0c967b0e683c63e1d96a1a
DamianWieszczak <damian.wieszczak@gmail.com>
2022-05-17 00:14:14 +0200

    Add posibility to create todo list and remove elements from list

--- app/controllers/todo_elements_controller.rb
+++ app/controllers/todo_elements_controller.rb
@@ -1,8 +1,18 @@
 class TodoElementsController < ApplicationController
   before_action :set_todo_list
-  
+
   def create
     @todo_element = @todo_list.todo_elements.create(todo_element_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    @todo_element = @todo_list.todo_elements.find(params[:id])
+    if @todo_element.destroy
+      flash[:notice] = "Your ToDo List element was removed"
+    else
+      flash[:alert] = "Your ToDo List element could not be removed"
+    end
     redirect_to @todo_list
   end
 
