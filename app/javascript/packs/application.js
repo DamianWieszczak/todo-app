74120a9a67ed32d1cda34c5f4cb977a8d1f10f4b
DamianWieszczak <damian.wieszczak@gmail.com>
2022-05-18 00:40:24 +0200

    Add bootstrap to the app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+var jQuery = require("jquery")
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require('bootstrap')
